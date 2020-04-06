e2086806175bcbb97f816ebe9049ccee6750377c
Daniel Middleton <daniel.middleton@skybettingandgaming.com>
2020-04-06 18:32:10 +0100

    first commit

--- stockspy/products/products.py
+++ stockspy/products/products.py
@@ -7,9 +7,9 @@ class Products():
         pass
 
     def load(self):
-        with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'products.json'), 'r') as products_file:
+        with open(os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                  '..', 'products.json'), 'r') as products_file:
+
             products_raw = products_file.read()
 
-        products = json.loads(products_raw)
-
-        return products
+        return json.loads(products_raw)
