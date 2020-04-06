c6aa6ffd3c283bb5f71b71c8d1c7f0c183ca661f
Daniel Middleton <daniel.middleton@skybettingandgaming.com>
2020-04-06 19:25:07 +0100

    first commit

--- stockspy/vendors/scrapers/oculus.py
+++ stockspy/vendors/scrapers/oculus.py
@@ -4,8 +4,8 @@ from .common import Vendor
 class Oculus(Vendor):
     @Vendor.scraper
     def get_stock(self, url):
-        # Specifically 60gb varient.
-        if '<span class="_7w4j"><span class="_7w4l">Not Available</span><sup>*</sup><span class="_7w4k"></span><span class="_7w54"><span class="_7w55">64</span><span class="_7w56">GB</span><sup class="_7w57">1</sup></span></span>' not in self.scraper.page_source:
+        # Specifically 64gb varient.
+        if 'Oculus Quest 64 GB is out of stock' not in self.scraper.page_source:
             return 1
         else:
             return 0
