802b25a04c28aafdce4d7d12771c8a6c62168105
Daniel Middleton <daniel.middleton@skybettingandgaming.com>
2020-04-09 21:36:27 +0100

    Automation

--- stockspy/vendors/scrapers/common.py
+++ stockspy/vendors/scrapers/common.py
@@ -22,11 +22,8 @@ class Vendor():
             options = ChromeOptions()
             options.add_argument('--headless')
             options.add_argument(f'user-agent={headers}')
-            # options.add_argument('--remote-debugging-port=9222')
-            # options.add_argument("user-data-dir=/Users/{username}/Library/Application Support/Google/Chrome/")
 
             self.scraper = webdriver.Chrome(options=options)
-            # self.scraper.set_window_size(1920, 1080)
 
             # Request page content.
             self.scraper.get(url)
