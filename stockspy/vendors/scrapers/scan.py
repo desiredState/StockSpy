48b99cf11ec02fc42490c310f70aeb8e903037ac
Daniel Middleton <daniel.middleton@skybettingandgaming.com>
2020-04-09 19:00:55 +0100

    Automation

--- stockspy/vendors/scrapers/scan.py
+++ stockspy/vendors/scrapers/scan.py
@@ -4,10 +4,9 @@ from .common import Vendor
 class Scan(Vendor):
     @Vendor.scraper
     def get_stock(self, url):
-        element = self.scraper.find_element_by_xpath('/html/body/div[2]/div[3]/div[2]/div[1]/div[2]/div[1]/div/div/div[3]/div/span/a')
+        element = self.scraper.find_element_by_xpath('/html/body/div[2]/div[3]/div[2]/div[1]/div[2]/div[1]/div/div/div[1]/span[2]')
 
-        if element.text == 'Pre Order':
-            print('found')
+        if 'Item due on' in element.text:
             return 0
         else:
             return 1
