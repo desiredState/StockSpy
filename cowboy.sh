b0b4b19beebfe3e3eafa62a4dafdee5c6873272f
Daniel Middleton <daniel.middleton@skybettingandgaming.com>
2020-04-09 14:41:59 +0100

    ok

--- cowboy.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker rm -f $(docker ps -aq) && ./factory.sh buildtest -x -i 5 -u dannydirect90@gmail.com -p ybkdbblyhhjxjhav && git add -A && git commit -m 'Automation' && git push && git status && docker ps
