bec209b97895573866f4281842950490068fa4b7
Daniel Middleton <daniel.middleton@skybettingandgaming.com>
2020-04-10 00:13:29 +0100

    Automation

--- stockspy/vendors/scrapers/oculus.py
+++ stockspy/vendors/scrapers/oculus.py
@@ -5,7 +5,7 @@ class Oculus(Vendor):
     @Vendor.scraper
     def get_stock(self, url):
         # Specifically 64gb varient.
-        element = self.scraper.find_element_by_xpath('//*[@id="pre-orderfeatures"]/div/div/div[9]/div[2]/div/span/span[1]')
+        element = self.scraper.find_element_by_xpath('//*[@id="pre-orderfeatures"]/div/div/div[2]/div[2]/div/span/span[1]')
 
         if element.text == 'Not Available':
             return 0
