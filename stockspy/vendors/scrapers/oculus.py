c580ab33c78c5651c86d7bbccbeeef6bf93e8792
Daniel Middleton <daniel.middleton@skybettingandgaming.com>
2020-04-09 17:42:40 +0100

    Automation

--- stockspy/vendors/scrapers/oculus.py
+++ stockspy/vendors/scrapers/oculus.py
@@ -1,11 +1,28 @@
+import sys
+import time
+
+from selenium.webdriver.common.by import By
+
 from .common import Vendor
 
 
 class Oculus(Vendor):
+    # @Vendor.scraper
+    # def get_stock(self, url):
+    #     # Specifically 64gb varient.
+    #     if 'Oculus Quest 64 GB is out of stock' not in self.scraper.page_source:
+    #         return 1
+    #     else:
+    #         return 0
+
     @Vendor.scraper
     def get_stock(self, url):
         # Specifically 64gb varient.
-        if 'Oculus Quest 64 GB is out of stock' not in self.scraper.page_source:
-            return 1
-        else:
+        element = self.scraper.find_element_by_xpath('//*[@id="pre-orderfeatures"]/div/div/div[2]/div[2]/div/span/span[1]')
+
+        if element.text == 'Not Available':
+            print('NOT AVAILABLE')
             return 0
+        else:
+            print('AVAILABLE')
+            return 1
