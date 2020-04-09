74c3629fed3e33e8a572bf5a9403697b10cd2dc4
Daniel Middleton <daniel.middleton@skybettingandgaming.com>
2020-04-09 23:31:14 +0100

    Automation

--- stockspy/vendors/vendors.py
+++ stockspy/vendors/vendors.py
@@ -25,35 +25,31 @@ class Vendors():
         self.very = Very()
         self.johnlewis = JohnLewis()
 
-    def get_vendor(self, url):
-        vendor = urlparse(url)
-        return vendor.hostname
-
     def get_stock(self, url):
-        vendor = self.get_vendor(url)
+        vendor = urlparse(url)
 
-        if vendor == 'www.amazon.co.uk':
+        if vendor.hostname == 'www.amazon.co.uk':
             stock = self.amazonuk.get_stock(url)
 
-        elif vendor == 'www.argos.co.uk':
+        elif vendor.hostname == 'www.argos.co.uk':
             stock = self.argos.get_stock(url)
 
-        elif vendor == 'www.currys.co.uk':
+        elif vendor.hostname == 'www.currys.co.uk':
             stock = self.currys.get_stock(url)
 
-        elif vendor == 'www.oculus.com':
+        elif vendor.hostname == 'www.oculus.com':
             stock = self.oculus.get_stock(url)
 
-        elif vendor == 'www.overclockers.co.uk':
+        elif vendor.hostname == 'www.overclockers.co.uk':
             stock = self.overclockersuk.get_stock(url)
 
-        elif vendor == 'www.scan.co.uk':
+        elif vendor.hostname == 'www.scan.co.uk':
             stock = self.scan.get_stock(url)
 
-        elif vendor == 'www.very.co.uk':
+        elif vendor.hostname == 'www.very.co.uk':
             stock = self.very.get_stock(url)
 
-        elif vendor == 'www.johnlewis.com':
+        elif vendor.hostname == 'www.johnlewis.com':
             stock = self.johnlewis.get_stock(url)
 
         return stock
