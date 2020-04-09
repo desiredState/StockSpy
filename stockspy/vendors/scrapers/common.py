2885718882e04ec2621211e47e827726ca98205a
Daniel Middleton <daniel.middleton@skybettingandgaming.com>
2020-04-09 20:17:08 +0100

    Automation

--- stockspy/vendors/scrapers/common.py
+++ stockspy/vendors/scrapers/common.py
@@ -20,17 +20,50 @@ class Vendor():
                        '/77.0.3865.90 Safari/537.36'}
 
             options = ChromeOptions()
-            options.add_argument('--headless')
+            # options.add_argument('--headless')
             options.add_argument(f'user-agent={headers}')
             # options.add_argument("user-data-dir=/Users/{username}/Library/Application Support/Google/Chrome/")
 
             self.scraper = webdriver.Chrome(options=options)
             # self.scraper.set_window_size(1920, 1080)
 
-            # Retrieve page content.
+            # Request page content.
             self.scraper.get(url)
 
-            # Run vendor-specific scraping function.
+            # Prevent scraper identification.
+            self.scraper.execute_script("""
+navigator.languages = ['en-US', 'en'];
+navigator.plugins = [1, 2, 3, 4, 5];
+
+Object.defineProperty(navigator, 'languages', {
+  get: function() {
+    return ['en-US', 'en'];
+  },
+});
+
+Object.defineProperty(navigator, 'plugins', {
+  get: function() {
+    // this just needs to have `length > 0`, but we could mock the plugins too
+    return [1, 2, 3, 4, 5];
+  },
+});
+
+const getParameter = WebGLRenderingContext.getParameter;
+WebGLRenderingContext.prototype.getParameter = function(parameter) {
+  if (parameter === 37445) {
+    return 'Intel Open Source Technology Center';
+  }
+
+  if (parameter === 37446) {
+    return 'Mesa DRI Intel(R) Ivybridge Mobile ';
+  }
+
+  return getParameter(parameter);
+};
+""")
+
+            # Run the vendor-specific scraping function which is wrapped by
+            # this decorator.
             stock = func(self, url)
 
             self.scraper.quit()
