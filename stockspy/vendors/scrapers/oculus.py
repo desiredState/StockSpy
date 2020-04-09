bdeae71b006eeb3b4306fcfbd01c34ef6b6ac9cf
Daniel Middleton <daniel.middleton@skybettingandgaming.com>
2020-04-09 17:48:52 +0100

    Automation

--- stockspy/vendors/scrapers/oculus.py
+++ stockspy/vendors/scrapers/oculus.py
@@ -1,20 +1,7 @@
-import sys
-import time
-
-from selenium.webdriver.common.by import By
-
 from .common import Vendor
 
 
 class Oculus(Vendor):
-    # @Vendor.scraper
-    # def get_stock(self, url):
-    #     # Specifically 64gb varient.
-    #     if 'Oculus Quest 64 GB is out of stock' not in self.scraper.page_source:
-    #         return 1
-    #     else:
-    #         return 0
-
     @Vendor.scraper
     def get_stock(self, url):
         # Specifically 64gb varient.
--- wrapper.sh
+++ wrapper.sh
@@ -14,4 +14,4 @@ if [[ "$UPDATE" = true ]] ; then
     docker pull "${NAMESPACE}/${IMAGE}:${TAG}"
 fi
 
-docker run -d --restart always "${NAMESPACE}/${IMAGE}:${TAG}" "${@}"
+docker run --name stockspy -d --restart always "${NAMESPACE}/${IMAGE}:${TAG}" "${@}"
