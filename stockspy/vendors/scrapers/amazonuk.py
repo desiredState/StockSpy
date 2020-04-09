53317765233be86f8ee6128cb89c002c4257afc4
Daniel Middleton <daniel.middleton@skybettingandgaming.com>
2020-04-09 18:35:39 +0100

    Automation

--- stockspy/vendors/scrapers/amazonuk.py
+++ stockspy/vendors/scrapers/amazonuk.py
@@ -4,7 +4,11 @@ from .common import Vendor
 class AmazonUK(Vendor):
     @Vendor.scraper
     def get_stock(self, url):
-        if 'Currently unavailable.' not in self.scraper.page_source:
-            return 1
-        else:
+        element = self.scraper.find_element_by_xpath('//*[@id="availability"]/span')
+
+        if element.text == 'Currently unavailable.':
+            print('not in stock')
             return 0
+        else:
+            print('in stock')
+            return 1
