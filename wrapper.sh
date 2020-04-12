7fa634c8c73639a2dca84ba93be8f7e519604f88
Daniel Middleton <daniel.middleton@skybettingandgaming.com>
2020-04-12 12:38:18 +0100

    Automation

--- wrapper.sh
+++ wrapper.sh
@@ -14,4 +14,4 @@ if [[ "$UPDATE" = true ]] ; then
     docker pull "${NAMESPACE}/${IMAGE}:${TAG}"
 fi
 
-docker run --name stockspy -d --restart always -p 5000:5000 -p 3000:3000 "${NAMESPACE}/${IMAGE}:${TAG}" "${@}"
+docker run --name stockspy -d --restart always -p 8080:5000 -p 80:3000 "${NAMESPACE}/${IMAGE}:${TAG}" "${@}"
