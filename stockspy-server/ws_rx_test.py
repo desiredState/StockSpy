418f6fd4823007c245796158e8bdc4de1d4c19ce
Daniel Middleton <daniel.middleton@skybettingandgaming.com>
2020-04-17 19:46:02 +0100

    Bad commit message.

--- stockspy-server/ws_rx_test.py
+++ stockspy-server/ws_rx_test.py
@@ -4,12 +4,10 @@ import json
 
 
 async def rx():
-    uri = "ws://127.0.0.1:8765"
+    uri = "ws://127.0.0.1:8080"
 
     async with websockets.connect(uri) as websocket:
-        while True:
-            results = await websocket.recv()
-            print(json.loads(results))
-            await asyncio.sleep(10)
+        results = await websocket.recv()
+        print(json.loads(results))
 
 asyncio.get_event_loop().run_until_complete(rx())
